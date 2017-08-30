f3e6f12e1730d2f051fdbcc70027768093fb17bd
Emily Wise <wise.m.emily@gmail.com>
2017-08-30 13:48:20 -0600

    Updated the FileReader

--- lib/file_reader.rb
+++ lib/file_reader.rb
@@ -1,17 +1,21 @@
 
-require 'pry'
-require './lib/dictionary'
+
 
 class FileReader
   attr_reader :file_content
+  attr_accessor :formatted_text
 
   def initialize
-    @file_name =
+    @file_content = file_content
+    @formatted_text = ""
+    @filename = ""
   end
 
   def read
     filename = ARGV[0]
-    File.read(filename)
+    @file_content = File.read(filename)
+    @formatted_text = file_content.gsub!(/[\n]/, '')
+    @formatted_text.gsub!(" ",'')
   end
 
 end
